a55c9be159e1f9ec581759db4162dbf728fe6294
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-10-12 16:20:17 -0700

    now counts my age!

--- src/components/AgeTimer.jsx
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from 'react';
-
-export defualt function AgeTimer() {
-  const birthday = new Date(2008, 08, 19);
-  
-  const calculateAge = () => {
-    const now = new D
-  }
-}
