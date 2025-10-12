1ac6cdc05bfd6dd8b3f361aa13095c479855cab7
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-10-12 15:54:52 -0700

    trying to display age?

---
+++ src/components/AgeTimer.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react';
+
+export defualt function AgeTimer() {
+  const birthday = new Date(2008, 08, 19);
+  
+  const calculateAge = () => {
+    const now = new D
+  }
+}
