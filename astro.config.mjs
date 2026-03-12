dc5315c3bd0e45d2e232f2d302d4b43f9e996ea4
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2026-03-12 12:49:03 -0700

    Fix readme?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
+// 
 export default defineConfig({
   site: 'https://PixelSaver.github.io',
   base: '/Saving-Pixels/',
+  markdown: {
+    rehypePlugins: [
+      rehypeHeadingIds,
+    ],
+  },
 
   vite: {
     plugins: [tailwindcss()]
